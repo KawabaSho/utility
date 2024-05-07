a446d3dc2c03286aa753c61618d3c9aee1d6e9ba
KawabaSho <76656897+KawabaSho@users.noreply.github.com>
2024-05-07 20:31:46 +0900

    Add files via upload

---
+++ ToleranceMath.m
@@ -0,0 +1,27 @@
+classdef ToleranceMath
+    properties
+        Value
+        eps
+    end
+
+    methods
+        function obj = ToleranceMath(Value,Tolerance)
+            % 適当な演算をします
+            % a == b eq(a,b)
+            arguments
+                Value 
+                Tolerance (1,1) {mustBeNonnegative} = 1e-6
+            end
+            obj.Value = Value;
+            obj.eps   = Tolerance;
+        end
+        function tf = eq(obj1,obj2)
+            if abs(obj1.Value - obj2.Value)...
+                    <= min([obj1.eps, obj2.eps])
+                tf = true;
+            else
+                tf = false;
+            end
+        end
+    end
+end
